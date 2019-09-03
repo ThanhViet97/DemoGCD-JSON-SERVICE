19abd048152f53009c1f2d18976488a32147cd84
Thanh Việt <thanhvietbghl@gmail.com>
2019-09-03 15:38:42 +0700

    Create project

---
+++ DemoLessonSix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoLessonSix
+//
+//  Created by VietPhan on 9/3/19.
+//  Copyright © 2019 Phan Thanh Việt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoLessonSixTests/DemoLessonSixTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoLessonSixTests.swift
+//  DemoLessonSixTests
+//
+//  Created by VietPhan on 9/3/19.
+//  Copyright © 2019 Phan Thanh Việt. All rights reserved.
+//
+
+import XCTest
+@testable import DemoLessonSix
+
+class DemoLessonSixTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
